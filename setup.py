b57d1ad076b39754eeed9a1b10ecbb55bcb11730
Joshua <joshua.prince@modelop.com>
2026-06-16 13:37:09 -0600

    update to not have pip in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import re
 import tarfile
 
-import pip
+import requests
 from setuptools import setup, Extension, find_packages
 from distutils.ccompiler import new_compiler
 from distutils.command.build_ext import build_ext
@@ -24,18 +24,7 @@ def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
 
-def pip_install(package):
-    pip.main(['install', package])
-
 def download_file(url, path):
-    try:
-        import requests
-    except ImportError:
-        pip_install('requests')
-        try:
-            import requests
-        except ImportError:
-            sys.stderr.write("Unable to download setup dependencies\n")
     if not os.path.isdir("vendor"):
         os.mkdir("vendor")
     with open(path, 'wb') as f:
